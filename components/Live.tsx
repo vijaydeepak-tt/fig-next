0ab5ea1316ab137190e6f43ece1761d51a242ddd
Vijayasekhar Deepak <vijayasekhar.deepak@walmart.com>
2024-03-03 12:55:38 +0530

    live components done with liveblocks

---
+++ components/Live.tsx
@@ -0,0 +1,189 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  useBroadcastEvent,
+  useEventListener,
+  useMyPresence,
+  useOthers,
+} from '@/liveblocks.config';
+import LiveCursors from './cursor/LiveCursors';
+import { useCallback, useEffect, useState } from 'react';
+import CursorChat from './cursor/CursorChat';
+import { CursorMode, CursorState, Reaction, ReactionEvent } from '@/types/type';
+import ReactionSelector from './reaction/ReactionButton';
+import FlyingReaction from './reaction/FlyingReaction';
+import useInterval from '@/hooks/useInterval';
+
+export default function Live() {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
+  });
+  const [reactions, setReactions] = useState<Reaction[]>([]);
+
+  const broadcast = useBroadcastEvent();
+
+  // removing the reactions from state once all done
+  useInterval(() => {
+    setReactions((prevReactions) =>
+      prevReactions.filter((pr) => pr.timestamp > Date.now() - 4000)
+    );
+  }, 1000);
+
+  // setting reaction to state in intervals
+  useInterval(() => {
+    if (cursorState.mode === CursorMode.Reaction && cursorState.isPressed) {
+      setReactions((prevReactions) =>
+        prevReactions.concat([
+          {
+            point: { x: cursor.x, y: cursor.y },
+            value: cursorState.reaction,
+            timestamp: Date.now(),
+          },
+        ])
+      );
+
+      // used to transfer the reactions to all users in the room
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value: cursorState.reaction,
+      });
+    }
+  }, 100);
+
+  // showing the reactions to all users in the room
+  useEventListener((eventData) => {
+    const event = eventData.event as ReactionEvent;
+    setReactions((prevReactions) =>
+      prevReactions.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now(),
+        },
+      ])
+    );
+  });
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    if (cursor == null || cursorState.mode !== CursorMode.ReactionSelector) {
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+      updateMyPresence({ cursor: { x, y } });
+    }
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    setCursorState({ mode: CursorMode.Hidden });
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback(
+    (event: React.PointerEvent) => {
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+      updateMyPresence({ cursor: { x, y } });
+      setCursorState((state: CursorState) =>
+        cursorState.mode === CursorMode.Reaction
+          ? { ...state, isPressed: true }
+          : state
+      );
+    },
+    [cursorState.mode, setCursorState]
+  );
+
+  const handlePointerUp = useCallback(
+    (event: React.PointerEvent) => {
+      setCursorState((state: CursorState) =>
+        cursorState.mode === CursorMode.Reaction
+          ? { ...state, isPressed: true }
+          : state
+      );
+    },
+    [cursorState.mode, setCursorState]
+  );
+
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: '',
+        });
+      } else if (e.key === 'Escape') {
+        updateMyPresence({
+          message: '',
+        });
+        setCursorState({
+          mode: CursorMode.Hidden,
+        });
+      } else if (e.key === 'e') {
+        setCursorState({
+          mode: CursorMode.ReactionSelector,
+        });
+      }
+    };
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener('keyup', onKeyUp);
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keyup', onKeyUp);
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [updateMyPresence]);
+
+  const setReaction = useCallback((reaction: string) => {
+    setCursorState({
+      mode: CursorMode.Reaction,
+      reaction,
+      isPressed: false,
+    });
+  }, []);
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      onPointerUp={handlePointerUp}
+      className='h-[100vh] w-full flex justify-center items-center text-center'
+    >
+      <h1 className='text-5xl text-white'>Fig Next</h1>
+
+      {reactions.map((reaction) => (
+        <FlyingReaction
+          key={reaction.timestamp.toString()}
+          x={reaction.point.x}
+          y={reaction.point.y}
+          timestamp={reaction.timestamp}
+          value={reaction.value}
+        />
+      ))}
+
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
+
+      {cursorState.mode === CursorMode.ReactionSelector && (
+        <ReactionSelector setReaction={setReaction} />
+      )}
+
+      <LiveCursors others={others} />
+    </div>
+  );
+}
---
+++ components/users/ActiveUsers.tsx
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react';
+import { Avatar } from './Avatar';
+import { useOthers, useSelf } from '../../liveblocks.config';
+import styles from './index.module.css';
+import { generateRandomName } from '@/lib/utils';
+
+const ActiveUsers = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > 3;
+
+  const memoizedUsers = useMemo(
+    () => (
+      <div className='flex justify-center items-center gap-1 py-2'>
+        <div className='flex pl-3'>
+          {currentUser && (
+            <Avatar
+              name='You'
+              otherStyles='border-[3px] border primary-green'
+            />
+          )}
+
+          {users.slice(0, 3).map(({ connectionId, info }) => {
+            return (
+              <Avatar
+                key={connectionId}
+                name={generateRandomName()}
+                otherStyles='-ml-3'
+              />
+            );
+          })}
+
+          {hasMoreUsers && (
+            <div className={styles.more}>+{users.length - 3}</div>
+          )}
+        </div>
+      </div>
+    ),
+    [users.length]
+  );
+
+  return memoizedUsers;
+};
+
+export default ActiveUsers;
